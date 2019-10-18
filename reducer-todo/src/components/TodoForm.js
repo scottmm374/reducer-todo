79cc07bc960f56653fa5a1424f53916aaf75c88c
Michelle Scott <scottmm374@gmail.com>
2019-10-18 10:59:13 -0400

    Push to check previous commits

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -1,27 +1,41 @@
-import React, { useState, useReducer } from 'react'
-import { initialState, reducer } from '../reducers/reducer'
+import React, { useState, useReducer } from "react";
+import { initialState, reducer } from "../reducers/reducer";
 
 const TodoForm = () => {
-    const[todo, setTodo] = useState({});
-    const[completed, setCompleted] = useState(false);
+  //   const [completed, setCompleted] = useState(false);
+  const [values, setValues] = useState({
+    task: ""
+  });
+  
 
+  const handleChange = e => {
+    // sets value, if setTodo commented out in handleSubmit. Otherwise it clears the value before submit.
+    setValues({...values, [e.target.name]: e.target.value});
+  };
 
-    return(
-        <div>
-            <form>
-             {/* onSubmit={handleSubmit}> */}
-                <input type='text'
-                         value="task" 
-                         placeholder="Add Todo" 
-                        //  onChange={handleChange}
-                          />
-            </form>
-            <div>
-                <button>Add Todo</button>
-                <button>Clear Completed</button>
-            </div>
-        </div>
-    )    
-}
+  const handleSubmit = e => {
+    e.preventDefault();
+    setValues({ task: "" });
+  };
+
+  console.log("task", values);
+//   console.log("todo", todo);
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="item"
+          value={values.task}
+          onChange={handleChange}
+        />
+      </form>
+      <div>
+        <button type="submit">Add Todo</button>
+        <button>Clear Completed</button>
+      </div>
+    </div>
+  );
+};
 
 export default TodoForm;
