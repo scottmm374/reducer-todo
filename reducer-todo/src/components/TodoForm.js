0dc59b2cde4b8614513699b4e2b7782e643399f8
Michelle Scott <scottmm374@gmail.com>
2019-10-18 12:19:10 -0400

    Testing things, only saves complete object for first input, then only saves task for each object after.

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -1,37 +1,47 @@
 import React, { useState, useReducer } from "react";
-import { initialState, reducer } from "../reducers/reducer";
+// import { initialState, reducer } from "../reducers/reducer";
 
 const TodoForm = () => {
-  //   const [completed, setCompleted] = useState(false);
-  const [values, setValues] = useState({
-    task: ""
+  // created empty array to store created Objects from input.
+  const [task, setTask] = useState([]);
+  const [input, setInput] = useState({
+    task: "",
+    id: Date.now(),
+    completed: false
   });
-  
 
+  //setting input values for Form
   const handleChange = e => {
-    // sets value, if setTodo commented out in handleSubmit. Otherwise it clears the value before submit.
-    setValues({...values, [e.target.name]: e.target.value});
+    setInput({ ...input, [e.target.name]: e.target.value });
   };
 
+  // Taking Input values and saving to task array
   const handleSubmit = e => {
     e.preventDefault();
-    setValues({ task: "" });
+    setTask([...task, input]);
+    // resetting input field. At this point pressing button does not reset the field, only hitting enter?
+    setInput({ task: "" });
+
+    // Below throws this error-- A component is changing a controlled input of type text to be uncontrolled. Input elements should not switch from controlled to uncontrolled (or vice versa).
+    //    Decide between using a controlled or uncontrolled input element for the lifetime of the component. More info: https://fb.me/react-controlled-components
+    //   setInput('')
   };
 
-  console.log("task", values);
-//   console.log("todo", todo);
+  console.log("task", task);
+  console.log("input", input);
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          name="item"
-          value={values.task}
+          name="task"
+          value={input.task}
           onChange={handleChange}
         />
       </form>
       <div>
-        <button type="submit">Add Todo</button>
+          {/* Had to add onClick with handle submit to get button to work? */}
+        <button onClick={handleSubmit}>Add Todo</button>
         <button>Clear Completed</button>
       </div>
     </div>
@@ -39,3 +49,32 @@ const TodoForm = () => {
 };
 
 export default TodoForm;
+
+// Commit Notes, for myself 
+
+// At this point Button click and hitting enter returns this in console,  
+// only first one returning the complete object, last two only returning the task. Onclick in button doing this?
+ 
+// [
+//     {
+//       "name": "State",
+//       "value": [
+//         {
+//           "task": "More stuff",
+//           "id": 1571415349401,
+//           "completed": false
+//         },
+//         {
+//           "task": "And more stuff"
+//         }
+//       ],
+//       "subHooks": []
+//     },
+//     {
+//       "name": "State",
+//       "value": {
+//         "task": ""
+//       },
+//       "subHooks": []
+//     }
+//   ]
